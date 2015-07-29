269ca2bb06cbe031d0c50685c3b9713208723a41
Ben <codeandcats@gmail.com>
2015-07-29 19:26:46 +1000

    Gulp goodness

---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var clean = require('gulp-clean');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+
+gulp.task('clean-client', function() {
+	return gulp
+		.src([
+			'public/**/*.js',
+			'public/**/*.map', 
+			'!public/lib/**/*.*'],
+			{ read: false })
+		.pipe(clean());
+});
+
+gulp.task('clean-server', function() {
+	return gulp
+		.src([
+			'**/*.js',
+			'**/*.map',
+			'!node_modules/**/*.*',
+			'!public/**/*.*',
+			'!gulpfile.js'],
+			{ read: false })
+		.pipe(clean());
+});
+
+var tsProject = ts.createProject('tsconfig.json', { noEmitOnError: false });
+
+gulp.task('build-client', ['clean-client'], function() {
+	var tsResult = gulp
+		.src([
+			'public/**/*.ts',
+			'typings/*.ts'],
+			{ base: '.' })
+		.pipe(ts(tsProject));
+		
+	var jsResult = tsResult
+		.js
+		.pipe(gulp.dest('.'));
+	
+	return jsResult;
+});
+
+gulp.task('build-server', ['clean-server'], function() {
+	var tsResult = gulp
+		.src([
+			'**/*.ts',
+			'!node_modules/**/*.*'],
+			{ base: '.' })
+		.pipe(ts(tsProject));
+		
+	var jsResult = tsResult
+		.js
+		.pipe(gulp.dest('.'));
+	
+	return jsResult;
+});
+
+gulp.task('copy-angular', ['build-client'], function() {
+	return gulp
+		.src([
+			'node_modules/angular/angular.js',
+			'node_modules/angular/angular.min.js',
+			'node_modules/angular-resource/angular-resource.js',
+			'node_modules/angular-resource/angular-resource.min.js',
+			'node_modules/angular-ui-router/build/angular-ui-router.js',
+			'node_modules/angular-ui-router/build/angular-ui-router.min.js'])
+		.pipe(gulp.dest('public/libs/angular'));
+});
+
+gulp.task('copy-bootstrap', ['build-client'], function() {
+	return gulp
+		.src('node_modules/bootstrap/dist/**/*.*')
+		.pipe(gulp.dest('public/libs/bootstrap'));
+});
+
+gulp.task('copy-jquery', ['build-client'], function() {
+	return gulp
+		.src([
+			'node_modules/jquery/dist/**/*.js',
+			'node_modules/jquery/dist/**/*.map'])
+		.pipe(gulp.dest('public/libs/jquery'))
+});
+
+gulp.task('copy-client-libs', ['copy-angular', 'copy-bootstrap', 'copy-jquery']);
+
+gulp.task('bundle-client', ['build-client', 'copy-client-libs'], function() {
+	return gulp
+		.src('public/scripts/app.js')
+		.pipe(browserify({
+			//ignore: ['angular', 'bootstrap', 'jquery'],
+			debug: true
+		}))
+		.pipe(rename('bundle.js'))
+		.pipe(gulp.dest('public/scripts'));
+});
+
+gulp.task('build', ['build-server', 'bundle-client']);
+
+gulp.task('default', ['build']);
